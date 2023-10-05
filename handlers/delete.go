163f02fd9f2366bf69e3b798b4c347ac94d9f73f
Leandro Portugal <leandroportugal@outlook.com>
2023-10-04 21:37:42 -0300

    Creates the delete function in the handlers package

---
+++ handlers/delete.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/leandro-portugal/MyTask.git/models"
+)
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+
+		log.Printf("Erro ao fazer parse do id %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Delete(int64(id))
+
+	if err != nil {
+
+		log.Printf("Erro ao deletar registro %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+
+	}
+	if rows > 1 {
+		log.Printf("Error: Foram deletados %d registros", rows)
+	}
+
+	response := map[string]any{
+		"error":   false,
+		"message": "Dados deletados com sucesso!",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+}
